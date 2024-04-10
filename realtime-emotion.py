c9b3144d7b105b4626137452bb67e286c371b0fd
Raihan-009 <islamraihan009@gmail.com>
2024-04-10 10:57:32 +0600

    refactor to encapsulate functionalities

--- realtime-emotion.py
+++ realtime-emotion.py
@@ -1,37 +1,45 @@
 import cv2
 from deepface import DeepFace
 
-face_cascade = cv2.CascadeClassifier('haar_face.xml')
-
-cam = cv2.VideoCapture(1)
-
-if not cam.isOpened():
-    cam = cv2.VideoCapture(0)
-if not cam.isOpened():
-    raise IOError("Cannot open webcam")
-
-while True:
-    ret, frame = cam.read()
-    res = DeepFace.analyze(frame, actions=("emotion"), enforce_detection=False)
+def detect_faces(frame):
+    face_cascade = cv2.CascadeClassifier('haar_face.xml')
     grayimg = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(grayimg, 1.1, 5)
-    for a,b,c,d in faces:
-        cv2.rectangle(frame, (a,b), (a+c,b+d), (255,0,0), 3)
+    return faces
+
+def draw_emotion_text(frame, face, emotion):
+    a, b, c, d = face
+    font_scale = 2.0  
+    text_size = cv2.getTextSize(emotion, cv2.FONT_HERSHEY_SIMPLEX, font_scale, 2)[0]
+    text_position = (a + c // 2 - text_size[0] // 2, b + d + 50)  
+    cv2.rectangle(frame, (text_position[0], text_position[1] - text_size[1]), 
+                  (text_position[0] + text_size[0], text_position[1]), (0, 0, 0), -1)
+    cv2.putText(frame, emotion, text_position, cv2.FONT_HERSHEY_SIMPLEX, font_scale, (255, 255, 255), 2)
+
+def main():
+    cam = cv2.VideoCapture(1)
+    if not cam.isOpened():
+        cam = cv2.VideoCapture(0)
+    if not cam.isOpened():
+        raise IOError("Cannot open webcam")
+    
+    while True:
+        ret, frame = cam.read()
+        res = DeepFace.analyze(frame, actions=("emotion"), enforce_detection=False)
+        faces = detect_faces(frame)
         
-        emotion = res[0]['dominant_emotion']
-        font_scale = 2.0  
-        text_size = cv2.getTextSize(emotion, cv2.FONT_HERSHEY_SIMPLEX, font_scale, 2)[0]
-        text_position = (a + c // 2 - text_size[0] // 2, b + d + 50)  
+        for face in faces:
+            cv2.rectangle(frame, (face[0], face[1]), (face[0] + face[2], face[1] + face[3]), (255,0,0), 3)
+            emotion = res[0]['dominant_emotion']
+            draw_emotion_text(frame, face, emotion)
 
-        cv2.rectangle(frame, (text_position[0], text_position[1] - text_size[1]), 
-                    (text_position[0] + text_size[0], text_position[1]), (0, 0, 0), -1)
-        cv2.putText(frame, emotion, text_position, cv2.FONT_HERSHEY_SIMPLEX, font_scale, (255, 255, 255), 2)
+        cv2.imshow("yayy", frame)
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        
+    cam.release()
+    cv2.destroyAllWindows()
 
-    
-    cv2.imshow("yayy", frame)
-    
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-    
-cam.release()
-cv2.destroyAllWindows()
+if __name__ == "__main__":
+    main()
