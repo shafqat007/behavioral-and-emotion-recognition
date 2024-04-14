e25718af186a4fa800b215336313251867cd3fc3
Raihan-009 <islamraihan009@gmail.com>
2024-04-14 22:27:03 +0600

    refined tracking system

--- app.py
+++ app.py
@@ -52,6 +52,11 @@ def lip_distance(shape):
         return 4
 
 def generate_frames():
+    sleepiness = 0
+    drowsiness = 0
+    awakeness = 0
+    activity = ""
+    color = (0,0,0)
     cap = cv2.VideoCapture(0)
 
     while True:
@@ -59,7 +64,8 @@ def generate_frames():
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         faces = face_detector(gray)
-
+        
+        
         if len(faces) > 0:
             for face in faces:
                 x1 = face.left()
@@ -67,41 +73,61 @@ def generate_frames():
                 x2 = face.right()
                 y2 = face.bottom()
 
-                landmarks = faceLandmarks(gray, face)
+                landmarks = faceLandmarks(gray,face)
+                #print(landmarks)
                 landmarks = face_utils.shape_to_np(landmarks)
-
-                left_eye = blinkingDetection(landmarks[36], landmarks[37], landmarks[38], landmarks[41], landmarks[40], landmarks[39])
-                right_eye = blinkingDetection(landmarks[42], landmarks[43], landmarks[44], landmarks[47], landmarks[46], landmarks[45])
+                #print(landmarks)
+                
+                left_eye = blinkingDetection(landmarks[36],landmarks[37],landmarks[38],landmarks[41],landmarks[40],landmarks[39])
+                #print(left_eye)
+                right_eye = blinkingDetection(landmarks[42],landmarks[43],landmarks[44],landmarks[47],landmarks[46],landmarks[45])
+                #print(right_eye)
                 mar = lip_distance(landmarks)
 
-                if left_eye == 0 or right_eye == 0 or mar == 0:
-                    activity = "Alert! Are you sleeping?"
-                    color = (0, 0, 255)
-                    # Play sound
-                    alert_sound.play()
-                elif left_eye == 1 or right_eye == 1 or mar == 3:
-                    activity = "Hushh! You looks sleepy!"
-                    color = (0, 0, 0)
-                    # Play sound
-                    alert_sound.play()
-                elif left_eye == 2 or right_eye == 2 and mar == 4:
-                    activity = "Having a safe driving!"
-                    color = (0, 255, 0)
-                elif left_eye == 2 or right_eye == 2 and mar == 3:
-                    activity = "Hushh! You looks sleepy!"
-                    color = (0, 255, 0)
-                    # Play sound
-                    alert_sound.play()
-
-                cv2.rectangle(frame, (10, 5), (445, 40), (255, 255, 255), -1)
-                cv2.putText(frame, activity, (15, 30), cv2.FONT_HERSHEY_COMPLEX, 1, color, 2)
+                if (left_eye == 0 or right_eye == 0 or mar == 0):
+                    sleepiness += 1
+                    drowsiness = 0
+                    awakeness = 0
+                    if (sleepiness>6):
+                        activity = "Alert! Are you sleeping?"
+                        color = (0,0,255)
+                        # Play sound
+                        alert_sound.play()
+                elif (left_eye == 1 or right_eye ==1 or mar == 3):
+                    sleepiness = 0
+                    drowsiness +=1 
+                    awakeness = 0
+                    if (drowsiness>6):
+                        activity = "Hushh! You looks sleepy!"
+                        color = (0,0,0)
+                        # Play sound
+                        alert_sound.play()
+                elif (left_eye == 2 or right_eye ==2 and mar == 4):
+                    drowsiness =0
+                    sleepiness =0
+                    awakeness +=1
+                    if (awakeness>6):
+                        activity = "Having a safe driving!"
+                        color = (0,255,0)
+                elif (left_eye == 2 or right_eye ==2 and mar == 3):
+                    drowsiness =0
+                    sleepiness +=1
+                    awakeness  =0
+                    if (sleepiness>1):
+                        activity = "Hushh! You looks sleepy!"
+                        color = (0,255,0)
+                        # Play sound
+                        alert_sound.play()
+            
         else:
+            print("Error!")
             activity = "Driver Distracted!"
-            color = (0, 0, 0)
+            color = (0,0,0)
             # Play sound
             alert_sound.play()
-            cv2.rectangle(frame, (10, 5), (445, 40), (255, 255, 255), -1)
-            cv2.putText(frame, activity, (15, 30), cv2.FONT_HERSHEY_COMPLEX, 1, color, 2)
+
+        cv2.rectangle(frame, (10,5),(445,40), (255,255,255), -1 )
+        cv2.putText(frame, activity, (15,30), cv2.FONT_HERSHEY_COMPLEX, 1, color ,2 )
 
         ret, buffer = cv2.imencode('.jpg', frame)
         frame = buffer.tobytes()
