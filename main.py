82ba175f519ab09ae525eceaeca0639226176c61
Raihan-009 <islamraihan009@gmail.com>
2024-04-16 00:28:54 +0600

    main.py updated

--- main.py
+++ main.py
@@ -6,13 +6,19 @@ def run_behavioral_tracking():
 
 def run_emotion_detection():
     subprocess.run(["python", "realtime-emotion.py"])
+    
+def run_custom_classification():
+    subprocess.run(["python", "custom-classifier.py"])
 
 if __name__ == "__main__":
     behavioral_tracking_process = multiprocessing.Process(target=run_behavioral_tracking)
     emotion_detection_process = multiprocessing.Process(target=run_emotion_detection)
+    custom_classifier_process = multiprocessing.Process(target=run_custom_classification)
 
     behavioral_tracking_process.start()
     emotion_detection_process.start()
+    custom_classifier_process.start()
 
     behavioral_tracking_process.join()
     emotion_detection_process.join()
+    custom_classifier_process.join()
