e347a49d184c7b0cd7d87c65a3defd836930fed4
Raihan-009 <islamraihan009@gmail.com>
2024-04-15 21:55:20 +0600

    cell phone detected

--- custom-classifier.py
+++ custom-classifier.py
@@ -2,21 +2,27 @@ import cv2
 from ultralytics import YOLO
 import numpy as np
 
-# Open the default camera (usually webcam)
 cap = cv2.VideoCapture(0)
 model = YOLO("yolov8m.pt")
 
-# Check if the camera opened successfully
+
+classes = []
+with open('classes.txt', "r") as file_object:
+    for class_name in file_object.readlines():
+        class_name = class_name.strip()
+        classes.append(class_name)
+        
+# print(classes)
+# print(type(classes))
+
 if not cap.isOpened():
     print("Error: Couldn't open camera")
     exit()
 
-# Loop to capture frames from the camera
 while True:
-    # Capture frame-by-frame
+    
     ret, frame = cap.read()
 
-    # If frame is read correctly ret is True
     if not ret:
         print("Error: Can't receive frame (stream end?). Exiting ...")
         break
@@ -26,18 +32,16 @@ while True:
     bboxes = np.array(result.boxes.xyxy.cpu(), dtype="int")
     classes = np.array(result.boxes.cls.cpu(), dtype="int")
     for cls, bbox in zip(classes, bboxes):
-        (x, y, x2, y2) = bbox
-        cv2.rectangle(frame, (x, y), (x2, y2), (0, 0, 225), 2)
-        cv2.putText(frame, str(cls), (x, y - 5), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 225), 2)
+        if cls == 67:
+            (x, y, x2, y2) = bbox
+            cv2.rectangle(frame, (x, y), (x2, y2), (0, 0, 225), 2)
+            cv2.putText(frame, "Cell Phone", (x, y - 5), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 225), 2)
     
     
-    # Display the resulting frame
     cv2.imshow('frame', frame)
 
-    # Press 'q' to exit the loop
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Release the capture
 cap.release()
 cv2.destroyAllWindows()
